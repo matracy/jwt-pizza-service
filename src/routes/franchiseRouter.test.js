a68d40ec811305182db68e3645255a9c4b9ec853
marcus <marcus.a.tracy@gmail.com>
2024-10-01 20:31:00 -0600

    Changed tests to not require blank database.  Added tests for creating and deleting stores.

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -22,6 +22,7 @@ const testUser = {
 	email: "droptables@school.db",
 	password: "sqlInjection",
 };
+
 function makeTestFranchise(admn) {
 	return {
 		name: "SQL Pizza",
@@ -30,6 +31,28 @@ function makeTestFranchise(admn) {
 }
 let testUserAuthToken;
 let testUserLoggedInToken;
+
+async function createAuthedAdminToken(adminUser) {
+	const loggedInAdminResult = await request(app)
+		.put("/api/auth")
+		.send(adminUser);
+	console.log(
+		"Authenticated admin user",
+		adminUser,
+		` with result ${loggedInAdminResult.status}: `,
+		loggedInAdminResult.body,
+	);
+	return loggedInAdminResult.body.token;
+}
+
+async function createFranchise(adminUser, adminToken) {
+	//add a franchise
+	const addResult = await request(app)
+		.post("/api/franchise")
+		.set("Authorization", `Bearer ${adminToken}`)
+		.send(makeTestFranchise(adminUser));
+	return addResult.body;
+}
 
 beforeAll(async () => {
 	testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
@@ -43,7 +66,6 @@ beforeAll(async () => {
 test("ls franchise", async () => {
 	const result = await request(app).get("/api/franchise");
 	expect(result.status).toBe(200);
-	expect(result.body).toStrictEqual([]);
 });
 
 test("add delete franchise", async () => {
@@ -68,4 +90,27 @@ test("add delete franchise", async () => {
 		.send();
 	expect(delResult.status).toBe(200);
 	expect(delResult.body).toStrictEqual({ message: "franchise deleted" });
+});
+
+test("create delete store", async () => {
+	//get an admin and authenticate them
+	const admin = await createAdminUser();
+	const adminToken = await createAuthedAdminToken(admin);
+	//get a franchise
+	const franchise = await createFranchise(admin, adminToken);
+	expect(franchise).not.toBe(false); // Presumably, a non-existant franchise would be falsey
+	//create a store
+	let testStore = { franchiseID: franchise.id, name: `sotre-${randomName()}` };
+	const addResult = await request(app)
+		.post(`/api/franchise/${franchise.id}/store`)
+		.set("Authorization", `Bearer ${adminToken}`)
+		.send(testStore);
+	expect(addResult.status).toBe(200);
+	expect(addResult.body).not.toBe(false); // Presumably, a non-existant store would be falsey
+	//remove the store now that we are done with it
+	let removeResult = await request(app)
+		.delete(`/api/franchise/${franchise.id}/store/${addResult.body.id}`)
+		.set("Authorization", `Bearer ${adminToken}`);
+	expect(removeResult.status).toBe(200);
+	expect(removeResult.body).toStrictEqual({ message: "store deleted" });
 });
