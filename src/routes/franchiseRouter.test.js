c0f15054b1d7a4099f688199d254209b8f1ff48b
marcus <marcus.a.tracy@gmail.com>
2024-10-01 20:55:47 -0600

    Refactored to extract some helpers from franchiseRouter.test.js

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -1,58 +1,15 @@
 const request = require("supertest");
 const app = require("../service");
-const { Role, DB } = require("../database/database.js");
-
-function randomName() {
-	return Math.random().toString(36).substring(2, 12);
-}
-
-async function createAdminUser() {
-	let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
-	user.name = randomName();
-	user.email = user.name + "@admin.com";
-
-	await DB.addUser(user);
-
-	user.password = "toomanysecrets";
-	return user;
-}
-
-const testUser = {
-	name: "Robert Tables",
-	email: "droptables@school.db",
-	password: "sqlInjection",
-};
-
-function makeTestFranchise(admn) {
-	return {
-		name: "SQL Pizza",
-		admins: [{ email: admn.email }],
-	};
-}
+const {
+	randomName,
+	createAdminUser,
+	testUser,
+	makeTestFranchise,
+	createAuthedAdminToken,
+	createFranchise,
+} = require("./routerTestingHelpers.js");
 let testUserAuthToken;
 let testUserLoggedInToken;
-
-async function createAuthedAdminToken(adminUser) {
-	const loggedInAdminResult = await request(app)
-		.put("/api/auth")
-		.send(adminUser);
-	console.log(
-		"Authenticated admin user",
-		adminUser,
-		` with result ${loggedInAdminResult.status}: `,
-		loggedInAdminResult.body,
-	);
-	return loggedInAdminResult.body.token;
-}
-
-async function createFranchise(adminUser, adminToken) {
-	//add a franchise
-	const addResult = await request(app)
-		.post("/api/franchise")
-		.set("Authorization", `Bearer ${adminToken}`)
-		.send(makeTestFranchise(adminUser));
-	return addResult.body;
-}
 
 beforeAll(async () => {
 	testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
@@ -80,6 +37,7 @@ test("add delete franchise", async () => {
 		.post("/api/franchise")
 		.set("Authorization", `Bearer ${adminToken}`)
 		.send(makeTestFranchise(adminUser));
+	console.log(`recieved result ${addResult.status} with body `, addResult.body);
 	expect(addResult.status).toBe(200);
 	expect(addResult.body).not.toBe({});
 	const franchiseID = addResult.body.id;
---
+++ src/routes/routerTestingHelpers.js
@@ -0,0 +1,56 @@
+const request = require("supertest");
+const app = require("../service");
+const { Role, DB } = require("../database/database.js");
+
+function randomName() {
+	return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+	let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
+	user.name = randomName();
+	user.email = user.name + "@admin.com";
+
+	await DB.addUser(user);
+
+	user.password = "toomanysecrets";
+	return user;
+}
+
+const testUser = {
+	name: "Robert Tables",
+	email: "droptables@school.db",
+	password: "sqlInjection",
+};
+
+function makeTestFranchise(admn) {
+	return {
+		name: `SQL Pizza - ${randomName()} edition.`,
+		admins: [{ email: admn.email }],
+	};
+}
+
+async function createAuthedAdminToken(adminUser) {
+	const loggedInAdminResult = await request(app)
+		.put("/api/auth")
+		.send(adminUser);
+	return loggedInAdminResult.body.token;
+}
+
+async function createFranchise(adminUser, adminToken) {
+	//add a franchise
+	const addResult = await request(app)
+		.post("/api/franchise")
+		.set("Authorization", `Bearer ${adminToken}`)
+		.send(makeTestFranchise(adminUser));
+	return addResult.body;
+}
+
+module.exports = {
+	randomName,
+	createAdminUser,
+	testUser,
+	makeTestFranchise,
+	createAuthedAdminToken,
+	createFranchise,
+};
