afbb604fc582cec99723f5faf8df91bdc73ce602
marcus <marcus.a.tracy@gmail.com>
2024-10-01 19:52:53 -0600

    Added tests for missing franchise and unauthenticated users

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -11,7 +11,7 @@ let testUserLoggedInToken;
 beforeAll(async () => {
 	testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
 	const registerRes = await request(app).post("/api/auth").send(testUser);
-	testUserAuthToken = registerRes.body.token;
+	const testUserAuthToken = registerRes.body.token;
 	//TODO make sure that the auth token is signed in, since those are apparently separate.
 	const loginResult = await request(app).put("/api/auth").send(testUser);
 	testUserLoggedInToken = loginResult.body.token;
@@ -23,4 +23,35 @@ test("fetch orders", async () => {
 		.set("Authorization", `Bearer ${testUserLoggedInToken}`);
 	expect(orderResponse.status).toBe(200);
 	expect(orderResponse.body.orders).toStrictEqual([]);
+});
+
+test("place empty order", async () => {
+	let testOrder = { items: [] };
+	const orderResponse = await request(app)
+		.get("/api/order")
+		.set("Authorization", `Bearer ${testUserLoggedInToken}`)
+		.send(testOrder);
+	expect(orderResponse.status).toBe(200);
+});
+
+test("order from bad franchise", async () => {
+	let testOrder = { franchiseID: -1, items: [] };
+	const orderResponse = await request(app)
+		.get("/api/order")
+		.set("Authorization", `Bearer ${testUserLoggedInToken}`)
+		.send(testOrder);
+	expect(orderResponse.status).toBe(200);
+});
+
+test("Order without authentication", async () => {
+	let testOrder = { items: [] };
+	const orderResponse = await request(app)
+		.get("/api/order")
+		.set("Authorization", "Bearer badTokenDoesNotExist")
+		.send(testOrder);
+	console.log(
+		`Order response gave ${orderResponse.status} with body`,
+		orderResponse.body,
+	);
+	expect(orderResponse.status).toBe(401);
 });
